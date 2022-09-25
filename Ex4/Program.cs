af969f2d4d705899d0523c6fd1b80c5a90a136c1
Saglara <nohaevass@yandex.ru>
2022-09-25 21:00:12 +0300

    Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N. 5 -> 1, 4, 9, 16, 25. 2 -> 1,4

---
+++ Ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+Console.Clear();
+
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine());
+int i=1;
+for (i=1;i<n+1;i++)
+{
+    int sqr=i*i;
+    Console.Write($" {sqr}");
+}
