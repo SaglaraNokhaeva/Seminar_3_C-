42ee2e7c15ef47b8349554dad20073cf46a5d853
Saglara <nohaevass@yandex.ru>
2022-09-25 19:24:19 +0300

    апишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.

---
+++ Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+//в которой находится эта точка.
+Console.WriteLine("Введите x");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y");
+int y = int.Parse(Console.ReadLine());
+if (x>0&&y>0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x<0&&y>0)
+{
+    Console.WriteLine("2 четверть");
+}
+else if (x<0&&y<0)
+{
+    Console.WriteLine("3 четверть");
+}
+else
+{
+    Console.WriteLine("4 четверть");
+}
